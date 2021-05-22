6d5e5c83a9a5778aaeecd5e5360177a7561ee0e6
hafsid <50413142+hafsid@users.noreply.github.com>
2021-05-22 16:32:52 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box,box2;
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    box = new Box(200,100,50,50);
+    box2 = new Box(100,50,50,100);
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    box.display();
+    box2.display();
+}
